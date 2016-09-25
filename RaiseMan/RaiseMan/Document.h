bc979ee06bd9c6155ecfc16a9009624de92a110e
liangfen <liangfen1206@gmail.com>
2016-09-25 08:57:04 +0800

    add 'RaiseMan' demo

---
+++ RaiseMan/RaiseMan/Document.h
@@ -0,0 +1,20 @@
+//
+//  Document.h
+//  RaiseMan
+//
+//  Created by liangfen on 16/9/24.
+//  Copyright © 2016年 Imagination Studio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument {
+    //NSMutableArray *employees;
+}
+
+@property (nonatomic, copy) NSMutableArray *employees;
+
+//- (void) setEmployees : (NSMutableArray *) a;
+
+@end
+
---
+++ RaiseMan/RaiseMan/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  RaiseMan
+//
+//  Created by liangfen on 16/9/24.
+//  Copyright © 2016年 Imagination Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (readwrite, copy) NSString *personName;
+@property (readwrite) float expectedRaise;
+
+
+@end
